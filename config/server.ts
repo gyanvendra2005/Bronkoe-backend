08689f6afa654e0fb49b6697f43f3666eda37aa1
Gyanvendra Singh <gyanvendras2004@gmail.com>
2026-07-13 10:42:26 +0530

    logs added in server

--- config/server.ts
+++ config/server.ts
@@ -1,13 +1,41 @@
+// import type { Core } from '@strapi/strapi';
+
+// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS')!,
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+// export default config;
+
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+console.log('RAW APP_KEYS EXISTS:', !!process.env.APP_KEYS);
+console.log(
+  'APP_KEYS LENGTH:',
+  process.env.APP_KEYS?.split(',').length
+);
+
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
   app: {
     keys: env.array('APP_KEYS')!,
   },
+
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool(
+      'WEBHOOKS_POPULATE_RELATIONS',
+      false
+    ),
   },
 });
 
